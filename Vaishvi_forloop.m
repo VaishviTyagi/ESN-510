7eff7e1422560926f2359656dd21a2d070799e4a
VaishviTyagi <140053484+VaishviTyagi@users.noreply.github.com>
2023-08-28 12:35:21 +0530

    Add files via upload

---
+++ Vaishvi_forloop.m
@@ -0,0 +1,19 @@
+%for i=2:100
+   % if rem(i,2)==0
+        %disp (i)
+    %end
+%end
+
+%i=0;
+%for n = 1:3
+    %x = 2*n^2 + n + 1;
+    %disp (x);
+    %i = i + x;
+    %disp(i)
+%end
+
+for n=1:5
+    if n>3
+        disp (n)
+    end
+end
